cc923f040085e52cb852bbc0ceb0bab49ce8b9e5
DrunkMoonWolf <zemerovanastya2017@gmail.com>
2023-04-06 16:15:57 +0500

    nice

---
+++ WpfApp1/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Connection
+    {
+        public static testDatabaseEntities1 Database { get; set; }
+    }
+}
